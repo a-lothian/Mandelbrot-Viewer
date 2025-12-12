836fb99ed48c863650b8366ece80f2b11c689345
a-lothian <alexlothian0@gmail.com>
2025-12-12 23:55:12 +1030

    opened window

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
+#include <stdbool.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <SDL2/SDL.h>
+
+bool running = true;
 
 int main() {
-    printf("Hello world\n");
+    SDL_Init(0);
+    SDL_Window* pwindow = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, SDL_WINDOW_RESIZABLE);
+
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+    }
 }
