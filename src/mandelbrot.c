bb88ae1b0b2c93fd359322a770ed967813d79a30
al0235 <118354636+al0235@users.noreply.github.com>
2025-12-17 22:13:44 +1030

    Added periodic distance checking optimization

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -2,11 +2,6 @@
 #include <stdio.h>
 #include <math.h>
 
-struct coord {
-    double x;
-    double y;
-};
-
 static inline int isKnownInside(double x0, double y0) {
     // test 1. If x0, y0 is within distance of 1/4 from point (-1,0)
     // it is guaranteed to be inside
@@ -25,6 +20,9 @@ static inline int isKnownInside(double x0, double y0) {
 
 // calculate if the point(x(n+1),y(n+1)) will escape given enough iterations
 // returns number of iterations required to escape bounding length of 4
+//
+// Optimization 1, isKnownInside(): if x,y is within known regions, it will not escape.
+// Optimization 2, checkInterval: if the distance between x,y and x,y from many steps ago is tiny, it will not escape.
 int calculateMandelbrot(double x0, double y0, int max_iterations) {
 
     if (isKnownInside(x0, y0)) return max_iterations;
@@ -32,8 +30,11 @@ int calculateMandelbrot(double x0, double y0, int max_iterations) {
     double x = 0.0;
     double y = 0.0;
 
-    double x_next;
-    double y_next;
+    double oldx = 0.0;
+    double oldy = 0.0;
+
+    const double epsilon = 1e-6;
+    const int checkInterval = 20;
 
     for (int i = 0; i < max_iterations; i++) {
         double x2 = x * x;
@@ -47,6 +48,18 @@ int calculateMandelbrot(double x0, double y0, int max_iterations) {
 
         y = (2.0 * xy) + y0;
         x = (x2 - y2) + x0;
+
+        // Periodic distance check
+        if (i % checkInterval == 0) {
+            double dx = x - oldx;
+            double dy = y - oldy;
+
+            if (dx * dx + dy * dy < epsilon) {
+                return max_iterations; // inside set
+            }
+            oldx = x;
+            oldy = y;
+        }
     }
 
     return max_iterations;
